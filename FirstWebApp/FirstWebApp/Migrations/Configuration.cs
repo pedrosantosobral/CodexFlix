3210fe79fd7952b3dd397ea0c39a8e48d44ab9e8
Pedro Santos <pedrosantosobral@gmail.com>
2020-11-11 16:39:49 +0000

    change ActionResults

---
+++ FirstWebApp/FirstWebApp/Migrations/Configuration.cs
@@ -0,0 +1,23 @@
+namespace FirstWebApp.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<FirstWebApp.Models.ApplicationDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(FirstWebApp.Models.ApplicationDbContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
